bd97f0ddc346e270d2fc9cc508d0aa93b706e3ad
iftakher99 <19tonmoy99@gmail.com>
2024-04-20 13:01:57 +0600

    moving to context system

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,13 +1,24 @@
+import { useContext } from 'react';
 import BookShow from './BookShow';
+import BookContext from '../context/book';
 
 function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BookContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <>
+      <div>
+        <h3>{count}</h3> <button onClick={incrementCount}>increment</button>
+      </div>
+      <div className="book-list">{renderedBooks}</div>
+    </>
+  );
 }
 
 export default BookList;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Provider } from './context/book.jsx';
 const el = document.getElementById('root');
 ReactDOM.createRoot(el).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
